c3560d13090f98e36a7f89341c921ae0a0a7b267
Auxile_Isaac <auxile.isaac@gmail.com>
2022-11-21 15:35:24 +0200

    sanity configuration done

--- medium_clone/pages/index.tsx
+++ medium_clone/pages/index.tsx
@@ -1,16 +1,17 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Header from "../components/Header"
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Header from "../components/Header" ;
+import {sanityClient, urlFor} from "../sanity";
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>  
-      <div>
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
         <div className="px-10 space-y-5">
           <h1 className="text-6xl max-w-xl font-serif">
           <span className="underline decoration-black decoration-4">
@@ -25,11 +26,26 @@ const Home: NextPage = () => {
         </div>
         <img 
         className="hidden md:inline-flex h-32 lg:h-full"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaBrBxWgpyq7HB8KyltTavLspfHSCovLpVyA&usqp=CAU"
+        src="https://cdn.iconscout.com/icon/free/png-256/medium-47-433328.png"
         alt=""/> 
-      </div>    
+      </div>  
+      {/*Posts  */}
     </div>
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = `*[_type=="post"]{
+    _id,
+    title,
+    author -> {
+    name,
+    image
+  },
+  description,
+  mainImage,
+  slug
+  }`;
+
+  const posts = await sanityClient
+};
